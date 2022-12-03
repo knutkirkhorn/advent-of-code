cbac788665a7a95594b2a3b5aae0f50a9c91e034
Knut Kirkhorn <knutkirk@hotmail.com>
2022-12-03 13:13:15 +0100

    Add day 3 solution

---
+++ days/03.js
@@ -0,0 +1,44 @@
+/* eslint-disable unicorn/prefer-spread */
+import {promises as fs} from 'node:fs';
+
+const fileContent = await fs.readFile('./days/03_input.txt', 'utf8');
+const rucksackContents = fileContent.split('\n').filter(line => line !== '');
+
+function convertCharToPriority(char) {
+	return char.codePointAt(0) >= 97
+		? char.codePointAt(0) - 96 // a -> z
+		: char.codePointAt(0) - 38; // A -> Z
+}
+
+const priorities = rucksackContents.map(rucksackContent => {
+	const firstHalf = rucksackContent.slice(0, rucksackContent.length / 2).split('');
+	const secondHalf = rucksackContent.slice(rucksackContent.length / 2, rucksackContent.length).split('');
+	const commonItemType = firstHalf.find(itemType => secondHalf.includes(itemType));
+	const itemPriority = convertCharToPriority(commonItemType);
+	return itemPriority;
+});
+const sumPriorities = priorities.reduce((previous, current) => previous + current, 0);
+console.log('Sum priorities:', sumPriorities);
+
+const groupsOfThree = [];
+
+for (let index = 0; index < rucksackContents.length; index += 3) {
+	groupsOfThree.push([
+		rucksackContents[index],
+		rucksackContents[index + 1],
+		rucksackContents[index + 2]
+	]);
+}
+
+const groupPriorities = groupsOfThree.map(group => {
+	const firstContents = group[0].split('');
+	const secondContents = group[1].split('');
+	const thirdContents = group[2].split('');
+	const commonItemType = firstContents
+		.filter(itemType => secondContents.includes(itemType))
+		.find(itemType => thirdContents.includes(itemType));
+	const priority = convertCharToPriority(commonItemType);
+	return priority;
+});
+const groupSumPriorities = groupPriorities.reduce((previous, current) => previous + current, 0);
+console.log('Sum group priorities', groupSumPriorities);
