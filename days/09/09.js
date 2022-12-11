319827bfd8b001815ae585218dd68352a16aca2d
Knut Kirkhorn <knutkirk@hotmail.com>
2022-12-11 23:49:49 +0100

    Add day 9 part 2 solution

--- days/09/09.js
+++ days/09/09.js
@@ -11,76 +11,119 @@ const movements = input
 	.map(line => line.split(' '));
 
 const head = {x: 0, y: 0};
-const tail = {x: 0, y: 0};
-const tailVisitedPositions = new Set();
+let tails = [{x: 0, y: 0}];
+let tailVisitedPositions = [new Set()];
 
 function updateTailPositionIfNeeded() {
-	// If two steps directly in one direction
-	if (tail.x === head.x) {
-		if (tail.y === head.y + 2) {
-			tail.y--;
-		} else if (tail.y === head.y - 2) {
-			tail.y++;
-		}
-	} else if (tail.y === head.y) {
-		if (tail.x === head.x + 2) {
-			tail.x--;
-		} else if (tail.x === head.x - 2) {
-			tail.x++;
-		}
-	}
+	for (const [index, tail] of Object.entries(tails)) {
+		const currentHead = Number(index) === 0 ? head : tails[index - 1];
 
-	// If two steps in one direction and one in another
-	if (tail.x === head.x - 1) {
-		if (tail.y === head.y - 2) {
-			tail.x++;
-			tail.y++;
-		} else if (tail.y === head.y + 2) {
-			tail.x++;
-			tail.y--;
+		// If two steps directly in one direction
+		if (tail.x === currentHead.x) {
+			if (tail.y === currentHead.y + 2) {
+				tail.y--;
+			} else if (tail.y === currentHead.y - 2) {
+				tail.y++;
+			}
+		} else if (tail.y === currentHead.y) {
+			if (tail.x === currentHead.x + 2) {
+				tail.x--;
+			} else if (tail.x === currentHead.x - 2) {
+				tail.x++;
+			}
 		}
-	} else if (tail.x === head.x + 1) {
-		if (tail.y === head.y - 2) {
-			tail.x--;
-			tail.y++;
-		} else if (tail.y === head.y + 2) {
-			tail.x--;
-			tail.y--;
+
+		// If two steps in one direction and one in another
+		if (tail.x === currentHead.x - 1) {
+			if (tail.y === currentHead.y - 2) {
+				tail.x++;
+				tail.y++;
+			} else if (tail.y === currentHead.y + 2) {
+				tail.x++;
+				tail.y--;
+			}
+		} else if (tail.x === currentHead.x + 1) {
+			if (tail.y === currentHead.y - 2) {
+				tail.x--;
+				tail.y++;
+			} else if (tail.y === currentHead.y + 2) {
+				tail.x--;
+				tail.y--;
+			}
+		} else if (tail.y === currentHead.y - 1) {
+			if (tail.x === currentHead.x - 2) {
+				tail.x++;
+				tail.y++;
+			} else if (tail.x === currentHead.x + 2) {
+				tail.x--;
+				tail.y++;
+			}
+		} else if (tail.y === currentHead.y + 1) {
+			if (tail.x === currentHead.x - 2) {
+				tail.x++;
+				tail.y--;
+			} else if (tail.x === currentHead.x + 2) {
+				tail.x--;
+				tail.y--;
+			}
 		}
-	} else if (tail.y === head.y - 1) {
-		if (tail.x === head.x - 2) {
-			tail.x++;
-			tail.y++;
-		} else if (tail.x === head.x + 2) {
-			tail.x--;
-			tail.y++;
+
+		// If two steps away in both directions
+		if (tail.x === currentHead.x + 2) {
+			if (tail.y === currentHead.y + 2) {
+				tail.x--;
+				tail.y--;
+			} else if (tail.y === currentHead.y - 2) {
+				tail.x--;
+				tail.y++;
+			}
+		} else if (tail.x === currentHead.x - 2) {
+			if (tail.y === currentHead.y + 2) {
+				tail.x++;
+				tail.y--;
+			} else if (tail.y === currentHead.y - 2) {
+				tail.x++;
+				tail.y++;
+			}
 		}
-	} else if (tail.y === head.y + 1) {
-		if (tail.x === head.x - 2) {
-			tail.x++;
-			tail.y--;
-		} else if (tail.x === head.x + 2) {
-			tail.x--;
-			tail.y--;
+
+		tailVisitedPositions[index].add(`${tail.x},${tail.y}`);
+	}
+}
+
+function simulateMovements() {
+	for (const movement of movements) {
+		const [direction] = movement;
+		const distance = Number(movement[1]);
+
+		for (let index = 0; index < distance; index++) {
+			// Move head
+			if (direction === 'R') head.x++;
+			if (direction === 'U') head.y++;
+			if (direction === 'L') head.x--;
+			if (direction === 'D') head.y--;
+
+			updateTailPositionIfNeeded();
 		}
 	}
 }
 
-for (const movement of movements) {
-	const [direction] = movement;
-	const distance = Number(movement[1]);
+simulateMovements();
 
-	for (let index = 0; index < distance; index++) {
-		// Move head
-		if (direction === 'R') head.x++;
-		if (direction === 'U') head.y++;
-		if (direction === 'L') head.x--;
-		if (direction === 'D') head.y--;
+console.log('Distinct positions tail visited:', tailVisitedPositions[tailVisitedPositions.length - 1].size);
 
-		updateTailPositionIfNeeded();
+// Reset tails and visited positions
+head.x = 0;
+head.y = 0;
+tails = [];
+tailVisitedPositions = [];
 
-		tailVisitedPositions.add(`${tail.x},${tail.y}`);
-	}
+// Add 9 tails
+for (let index = 0; index < 9; index++) {
+	tails.push({x: 0, y: 0});
+	tailVisitedPositions.push(new Set());
 }
 
-console.log('Distinct positions tail visited:', tailVisitedPositions.size);
+simulateMovements();
+
+console.log('Distinct positions tail #9 visited:', tailVisitedPositions[tailVisitedPositions.length - 1].size);
