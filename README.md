28a4bd29f65c42b765baf4a48bfc9b89004d78f1
Knut Kirkhorn <knutkirk@hotmail.com>
2022-12-11 19:16:55 +0100

    Add day 9 part 1 solution

--- README.md
+++ README.md
@@ -29,5 +29,6 @@ npm run dev
 - [Day 6: Tuning Trouble](days/06)
 - [Day 7: No Space Left On Device](days/07)
 - [Day 8: Treetop Tree House](days/08)
+- [Day 9: Rope Bridge](days/09)
 - [Day 10: Cathode-Ray Tube](days/10)
 - [Day 11: Monkey in the Middle](days/11)
---
+++ days/09/09.js
@@ -0,0 +1,89 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {readInput} from '../../util.js';
+
+const directoryPath = path.dirname(fileURLToPath(import.meta.url));
+const input = await readInput(directoryPath);
+
+const movements = input
+	.trim()
+	.split('\n')
+	.map(line => line.split(' '));
+
+const head = {x: 0, y: 0};
+const tail = {x: 0, y: 0};
+
+const tailVisitedPositions = new Set();
+const tailVisitedPositionsArray = [];
+
+function updateTailPositionIfNeeded() {
+	// If two steps directly in one direction
+	if (tail.x === head.x) {
+		if (tail.y === head.y + 2) {
+			tail.y--;
+		} else if (tail.y === head.y - 2) {
+			tail.y++;
+		}
+	} else if (tail.y === head.y) {
+		if (tail.x === head.x + 2) {
+			tail.x--;
+		} else if (tail.x === head.x - 2) {
+			tail.x++;
+		}
+	}
+
+	// If two steps in one direction and one in another
+	if (tail.x === head.x - 1) {
+		if (tail.y === head.y - 2) {
+			tail.x++;
+			tail.y++;
+		} else if (tail.y === head.y + 2) {
+			tail.x++;
+			tail.y--;
+		}
+	} else if (tail.x === head.x + 1) {
+		if (tail.y === head.y - 2) {
+			tail.x--;
+			tail.y++;
+		} else if (tail.y === head.y + 2) {
+			tail.x--;
+			tail.y--;
+		}
+	} else if (tail.y === head.y - 1) {
+		if (tail.x === head.x - 2) {
+			tail.x++;
+			tail.y++;
+		} else if (tail.x === head.x + 2) {
+			tail.x--;
+			tail.y++;
+		}
+	} else if (tail.y === head.y + 1) {
+		if (tail.x === head.x - 2) {
+			tail.x++;
+			tail.y--;
+		} else if (tail.x === head.x + 2) {
+			tail.x--;
+			tail.y--;
+		}
+	}
+}
+
+for (const movement of movements) {
+	const [direction] = movement;
+	const distance = Number(movement[1]);
+
+	for (let index = 0; index < distance; index++) {
+		// Move head
+		if (direction === 'R') head.x++;
+		if (direction === 'U') head.y++;
+		if (direction === 'L') head.x--;
+		if (direction === 'D') head.y--;
+
+		updateTailPositionIfNeeded();
+
+		tailVisitedPositions.add(`${tail.x},${tail.y}`);
+		tailVisitedPositionsArray.push(`${tail.x},${tail.y}`);
+	}
+}
+
+console.log('Distinct positions tail visited:', tailVisitedPositions.size);
