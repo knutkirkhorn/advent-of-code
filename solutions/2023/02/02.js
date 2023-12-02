623558bd778a62edfe52cb82860c120e3f429132
Knut Kirkhorn <knutkirk@hotmail.com>
2023-12-02 14:56:08 +0100

    Add 2023 day 2 part one solution

---
+++ solutions/2023/02/02.js
@@ -0,0 +1,64 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {readInput, sum} from '../../../util.js';
+
+const directoryPath = path.dirname(fileURLToPath(import.meta.url));
+const input = await readInput(directoryPath);
+
+const gameRegex = /Game (\d+): (.+)/;
+const games = input
+	.trim()
+	.split('\n')
+	.map(line => {
+		const regexMatch = line.match(gameRegex);
+		const gameId = Number(regexMatch[1]);
+		const gameCubeSets = regexMatch[2]
+			.split(';')
+			.map(set => {
+				const splittedSet = set.split(',');
+				const cubes = splittedSet.map(cube => {
+					const splittedCube = cube.trim().split(' ');
+					return {[splittedCube[1]]: Number(splittedCube[0])};
+				});
+				const combinedCubes = {};
+
+				for (const cube of cubes) {
+					const color = Object.keys(cube)[0];
+					const number = cube[color];
+					combinedCubes[color] = number;
+				}
+				return combinedCubes;
+			});
+		return {
+			gameId,
+			gameCubeSets,
+		};
+	});
+
+const maxCubeCheck = {
+	red: 12,
+	green: 13,
+	blue: 14,
+};
+const possibleGameIds = [];
+
+for (const game of games) {
+	let gameIsImpossible = false;
+
+	for (const gameCubeSet of game.gameCubeSets) {
+		for (const color of Object.keys(gameCubeSet)) {
+			if (gameCubeSet[color] > maxCubeCheck[color]) {
+				gameIsImpossible = true;
+				break;
+			}
+		}
+
+		if (gameIsImpossible) break;
+	}
+
+	if (gameIsImpossible) continue;
+	possibleGameIds.push(game.gameId);
+}
+
+const sumPossibleGamesIds = sum(possibleGameIds);
+console.log('Sum possible game IDs:', sumPossibleGamesIds);
