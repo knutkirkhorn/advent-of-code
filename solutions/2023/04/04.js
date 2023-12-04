4247602e0fa4e23e64d9e0a808c1629649a10192
Knut Kirkhorn <knutkirk@hotmail.com>
2023-12-04 22:44:47 +0100

    Add 2023 day 4 part two solution

--- solutions/2023/04/04.js
+++ solutions/2023/04/04.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
-import {readInput} from '../../../util.js';
+import {readInput, sum} from '../../../util.js';
 
 const directoryPath = path.dirname(fileURLToPath(import.meta.url));
 const input = await readInput(directoryPath);
@@ -38,3 +38,24 @@ for (const scratchCard of scratchCards) {
 }
 
 console.log('The cards are worth points:', totalPoints);
+
+const numberOfEachCard = {};
+
+for (const [index, scratchCard] of scratchCards.entries()) {
+	const currentGameIndex = index + 1;
+	numberOfEachCard[currentGameIndex] = numberOfEachCard[currentGameIndex] || 1;
+	const numberOfCurrentCard = numberOfEachCard[currentGameIndex];
+	let matchingNumbers = 0;
+
+	for (const placedNumber of scratchCard.placedNumbers) {
+		if (scratchCard.winningNumbers.includes(placedNumber)) {
+			matchingNumbers++;
+			const wonCardIndex = currentGameIndex + matchingNumbers;
+			const currentNumberOfWonCard = numberOfEachCard[wonCardIndex] || 1;
+			numberOfEachCard[wonCardIndex] = currentNumberOfWonCard + numberOfCurrentCard;
+		}
+	}
+}
+
+const numberOfCards = sum(Object.values(numberOfEachCard));
+console.log('Total scratchcards:', numberOfCards);
