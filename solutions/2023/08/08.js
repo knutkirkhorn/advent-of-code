a24cde8a0adcf7c927c27f399bc6892c0ac25c0c
Knut Kirkhorn <knutkirk@hotmail.com>
2023-12-09 01:39:31 +0100

    Add 2023 day 8 part two solution

--- solutions/2023/08/08.js
+++ solutions/2023/08/08.js
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
+import gcd from '@knutkirkhorn/gcd';
 import {readInput} from '../../../util.js';
 
 const directoryPath = path.dirname(fileURLToPath(import.meta.url));
@@ -36,3 +37,36 @@ while (currentNode !== 'ZZZ') {
 }
 
 console.log('Steps required to reach ZZZ:', steps);
+
+const startingOnANodes = Object.keys(nodes).filter(node => node.endsWith('A'));
+steps = 0;
+const endingWithZSteps = [];
+
+for (const node of startingOnANodes) {
+	let currentSteps = 0;
+	let currentStartingOnANode = node;
+
+	while (!currentStartingOnANode.endsWith('Z')) {
+		for (const direction of directions) {
+			currentStartingOnANode = direction === 'R'
+				? nodes[currentStartingOnANode].right
+				: nodes[currentStartingOnANode].left;
+			currentSteps++;
+			if (currentStartingOnANode.endsWith('Z')) {
+				break;
+			}
+		}
+	}
+
+	endingWithZSteps.push(currentSteps);
+}
+
+const sortedEndingWithZSteps = endingWithZSteps.sort((a, b) => a - b);
+
+function leastCommonMultiple(a, b) {
+	return (a * b) / gcd(a, b);
+}
+
+// eslint-disable-next-line unicorn/no-array-reduce
+steps = sortedEndingWithZSteps.reduce((a, b) => leastCommonMultiple(a, b), sortedEndingWithZSteps[0]);
+console.log('Steps required for all nodes to end on Z:', steps);
